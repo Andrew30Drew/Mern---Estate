3951e3d252ee8c336bec47ca6abdf15db09bea23
IT22087874 <it22087874@my.sliit.lk>
2025-02-28 15:55:50 +0530

    create a middleware and function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js'
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("Connected to MongoDB");
-  }).catch(err => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
     console.log(err.message);
-});
+  });
 
 const app = express();
 
@@ -22,3 +24,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
